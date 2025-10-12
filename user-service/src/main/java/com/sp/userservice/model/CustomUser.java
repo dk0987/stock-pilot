e3cc5c89cecb8e61db2aeab48d480d4c50460f23
dk0987 <ydk0888@gmail.com>
2025-10-12 19:25:44 +0900

    Create CustomUser.java

---
+++ user-service/src/main/java/com/sp/userservice/model/CustomUser.java
@@ -0,0 +1,132 @@
+package com.sp.userservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class CustomUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+//   User ID
+    private UUID id;
+
+    @NotNull
+    @Column(unique = true)
+//   User Name
+    private String username ;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+//   User email
+    private String email;
+
+    @NotNull
+//    User Password
+    private String password;
+
+
+    @NotNull
+//    User First name
+    private String first_name;
+
+//    User last name
+    private String last_name;
+//    User phone number
+    private String phone_number;
+//    User account status
+    private boolean is_active;
+//    User created on
+    private LocalDate created_at;
+//    User ID modified on
+    private LocalDate updated_at;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPhone_number() {
+        return phone_number;
+    }
+
+    public void setPhone_number(String phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public boolean isIs_active() {
+        return is_active;
+    }
+
+    public void setIs_active(boolean is_active) {
+        this.is_active = is_active;
+    }
+
+    public LocalDate getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDate created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDate getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDate updated_at) {
+        this.updated_at = updated_at;
+    }
+
+
+
+
+
+}
