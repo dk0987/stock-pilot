9e0f925b4ee017dcb67542e84cab3b2b6c14c705
dk0987 <ydk0888@gmail.com>
2025-10-27 21:55:03 +0900

    Created Sales orders entities

--- sales-order-service/src/main/java/com/sp/salesorderservice/model/SalesOrderItems.java
+++ sales-order-service/src/main/java/com/sp/salesorderservice/model/SalesOrderItems.java
@@ -1,4 +1,81 @@
 package com.sp.salesorderservice.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "sales-order-item")
 public class SalesOrderItems {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    @NotNull
+    private UUID salesOrderId;
+    @NotNull
+    private UUID productId;
+    @NotNull
+    private int orderQuantity;
+    private int shippedQuantity;
+    @NotNull
+    private double unitPrice;
+    private double totalPrice;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getSalesOrderId() {
+        return salesOrderId;
+    }
+
+    public void setSalesOrderId(UUID salesOrderId) {
+        this.salesOrderId = salesOrderId;
+    }
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public void setProductId(UUID productId) {
+        this.productId = productId;
+    }
+
+    public int getOrderQuantity() {
+        return orderQuantity;
+    }
+
+    public void setOrderQuantity(int orderQuantity) {
+        this.orderQuantity = orderQuantity;
+    }
+
+    public int getShippedQuantity() {
+        return shippedQuantity;
+    }
+
+    public void setShippedQuantity(int shippedQuantity) {
+        this.shippedQuantity = shippedQuantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
 }
